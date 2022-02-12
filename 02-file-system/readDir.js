5859f5555cf43965372bdc273b280f09d8252e7b
Heidelol <heideloliver@gmail.com>
2022-02-11 20:26:22 -0500

    entendiendo data

--- 02-file-system/readDir.js
+++ 02-file-system/readDir.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-fs.readdir('../02-file-system','utf-8',(err) =>{
+fs.readdir('../02-file-system','utf-8',(err, data) =>{
     if(err){
         console.log('no se puede leer')
         return
     } else {
-        console.log('se pudo leer')
+        console.log('se pudo leer', data)
     }
 })
