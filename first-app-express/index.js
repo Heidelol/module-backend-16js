796df1103a194c7ee09a81ea4d2c0bee43dbf57c
Heidelol <heideloliver@gmail.com>
2022-02-18 23:03:57 -0500

    gracias fer

--- first-app-express/index.js
+++ first-app-express/index.js
@@ -17,13 +17,92 @@ server.use(express.json()) // recibir json en nuestro request
 //     })
 // })
 
-server.get('/koders',(request, response) => {
-   
-  
+// server.get('/koders', async (request, response) => {
+// //    fs.readFile() //callbacks
+// //    fs.promises.readFile() // promesa
+//     // const content = await fs.promises.readFile('./koders.json')
+
+//     const content = fs.readFileSync('./koders.json', 'utf8') //sincrona
+//     console.log(content) // string
+//     const json = JSON.parse(content) // convertir de string a un objeto valido
+//     console.log('---JSON---')
+//     console.log(json)
+//     response.json(json.koders)
+// })
+
+server.post('/koders', async (request, response) => {
+    const newKoder = request.body
+    console.log(newKoder)
+    const  content = await fs.promises.readFile('./koders.json')
+    const json = JSON.parse(content)
+    json.koders.push(newKoder)
+
+    console.log(json)
+
+    await fs.promises.writeFile('./koders.json', JSON.stringify(json, null, 1), 'utf8')
+
     response.json({
-        message: 'oki'
+        success: true,
+        message: 'Se creo el Koder'
     })
 })
+
+//update
+// sintaxys universal
+// path parameters
+//Patch /koders/1
+//Patch /koders/4
+//patch /recursos/identificador
+server.patch('/koders/:id', async (request, response) => {
+   console.log('params: ', request.params.id)
+   const idKoder = request.params.id
+   const name = request.body.name
+   const content = await fs.promises.readFile('./koders.json')
+   const json = JSON.parse(content)
+   console.log('json: ', json)
+
+   //map
+   const newKoders = json.koders.map((koder, index) => {
+        if(koder.id === parseInt(idKoder)) {
+            koder.name = name
+        }
+        return koder
+   })
+   console.log(newKoders)
+
+   json.koders = newKoders
+
+   await fs.promises.writeFile('./koders.json', JSON.stringify(json, null, 2),'utf8')
+   response.json({
+       success: true,
+   })
+})
+
+server.delete('/koders/:id', async (request, response) => {
+    console.log('params: ', request.params.id)
+    const idKoder = request.params.id
+    const name = request.body.name
+    const content = await fs.promises.readFile('./koders.json')
+    const json = JSON.parse(content)
+    console.log('json: ', json)
+ 
+    //map
+    const newKoders = json.koders.map((koder, index) => {
+         if(koder.id === parseInt(idKoder)) {
+             koder.name = name
+         }
+         return koder
+    })
+    console.log(newKoders)
+ 
+    json.koders = newKoders
+ 
+    
+    await fs.promises.unlink('./koders/:id', JSON.stringify(json, null, 2),'utf8')
+    response.json({
+        success: true,
+    })
+ })
 
 
 
@@ -39,6 +118,7 @@ server.listen(8080, () => {
     post /koders
     patch /koders
 */
+
 
 
 
